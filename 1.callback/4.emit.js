11c488b547d71feebc648c95cb1b0e20d9af2b51
fandonglai <704072802@qq.com>
2019-03-16 10:39:14 +0800

    callback

--- 1.callback/4.emit.js
+++ 1.callback/4.emit.js
@@ -1,8 +1,6 @@
 let fs = require('fs'); 
 // 发布 订阅  [fn,fn]
-function EventEmitter(){
-    this._arr = [];
-}
+function EventEmitter(){ this._arr = [];}
 EventEmitter.prototype.on = function(callback){ // 订阅
     this._arr.push(callback); // this._arr= [on1,on2]
 }
@@ -11,15 +9,15 @@ EventEmitter.prototype.emit = function(){ // 发布  发布时 需要让on的方
 }
 let e = new EventEmitter();
 let school = {}
-e.on(function(data,key){
-   console.log('一个接口成功')
+e.on(function(){
+   console.log('一个接口成功');
 })
 e.on(function(data,key){
     school[key] = data;
     if(Object.keys(school).length === 2){
         console.log(school);
     }
-})
+});
 fs.readFile('./name.txt','utf8',function(err,data){ 
     if(err) return console.log(err);
     e.emit(data,'name');
--- 1.callback/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-let fn1 = function(){console.log('fn1')};
-let fn2 = function(){console.log('fn2')};
-let arr = [fn1,fn2]
-
-arr.forEach(fn=>fn());
